90fad7b00264d30025cf58ea8699ad2e7e2f0918
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2025-01-10 23:47:30 -0400

    Add filters, pagination, sort and query objects for Movie and TvSerie entities

---
+++ MovieStream.Core.Application/Enums/OrderByOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStream.Core.Application.Enums
+{
+    public enum OrderByOptions
+    {
+        [Display(Name = "sort by...")] SimpleOrder = 0,
+        [Display(Name = "Publication Date ↑")] ByPublicationDate,
+    }
+}
---
+++ MovieStream.Core.Application/QueryObjects/MovieSort.cs
@@ -0,0 +1,24 @@
+﻿using MovieStream.Core.Application.Enums;
+using MovieStream.Core.Domain.Entities;
+
+namespace MovieStream.Core.Application.QueryObjects
+{
+    public static class MovieSort
+    {
+        public static IQueryable<Movie> OrderSeriesBy(this IQueryable<Movie> movies, OrderByOptions orderByOptions)
+        {
+            switch (orderByOptions)
+            {
+                case OrderByOptions.SimpleOrder:
+                    return movies.OrderByDescending(
+                        x => x.Id);
+                case OrderByOptions.ByPublicationDate:
+                    return movies.OrderByDescending(
+                        x => x.ReleaseDate);
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        nameof(orderByOptions), orderByOptions, null);
+            }
+        }
+    }
+}
