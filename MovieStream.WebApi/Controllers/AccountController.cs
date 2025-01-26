040eb1a518ce410d39237914d94ad5fc62418162
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2025-01-25 22:38:53 -0400

    Add AccountController for authentication, registration, and password management endpoints

---
+++ MovieStream.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieStream.Core.Application.DTOs.Account;
+using MovieStream.Core.Application.Enums;
+using MovieStream.Core.Application.Interfaces.Services;
+
+namespace MovieStream.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> AuthenticateAsync(AuthenticationRequest request)
+        {
+            return Ok(await _accountService.AuthenticateAsync(request));
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.RegisterBasicUserAsync(request, origin));
+        }
+
+        [Authorize(Roles = nameof(Roles.Admin))]
+        [HttpPost("register-content-manager")]
+        public async Task<IActionResult> RegisterContentManagerAsync(RegisterRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.RegisterContentManagerUserAsync(request, origin));
+        }
+
+        [Authorize(Roles = nameof(Roles.Admin))]
+        [HttpPost("register-admin")]
+        public async Task<IActionResult> RegisterAdminAsync(RegisterRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.RegisterAdminUserAsync(request, origin));
+        }
+
+        [HttpGet("confirm-email")]
+        public async Task<IActionResult> ConfirmAccountAsync([FromQuery] string userId, [FromQuery] string token)
+        {
+            return Ok(await _accountService.ConfirmAccountAsync(userId, token));
+        }
+
+
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPasswordAsync(ForgotPasswordRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.ForgotPasswordAsync(request, origin));
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPasswordAsync(ResetPasswordRequest request)
+        {
+            return Ok(await _accountService.ResetPasswordAsync(request));
+        }
+    }
+}
