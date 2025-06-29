b67c9a68e8367d7953d4162bc3930266edf16063
AleGxrcia <fagarciatavarez@gmail.com>
2025-06-29 18:40:33 -0400

    initialize React Router configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import { Layout } from './components/layout/Layout.tsx';
+import { RouterProvider } from 'react-router/dom';
+import router from './routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Bienvenido a MovieStream</h1>
+    </div>
+  )
+}
+
+export default HomePage;
