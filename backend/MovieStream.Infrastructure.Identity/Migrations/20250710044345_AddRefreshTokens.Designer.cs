41fab854c37036c55e215c9d7110567047cf576c
AleGxrcia <fagarciatavarez@gmail.com>
2025-07-10 02:18:43 -0400

    Add migration for RefreshToken entity and relationships

--- backend/MovieStream.Infrastructure.Identity/Migrations/20250710044345_AddRefreshTokens.Designer.cs
+++ backend/MovieStream.Infrastructure.Identity/Migrations/20250710044345_AddRefreshTokens.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieStream.Infrastructure.Identity.Contexts;
 
@@ -11,9 +12,11 @@ using MovieStream.Infrastructure.Identity.Contexts;
 namespace MovieStream.Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20250710044345_AddRefreshTokens")]
+    partial class AddRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -248,6 +251,7 @@ namespace MovieStream.Infrastructure.Identity.Migrations
                         .HasColumnType("datetime2");
 
                     b.Property<string>("ReplacedByToken")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime?>("Revoked")
---
+++ backend/MovieStream.Infrastructure.Identity/Migrations/20250710050516_UpdateRefreshTokens.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieStream.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRefreshTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReplacedByToken",
+                schema: "Identity",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReplacedByToken",
+                schema: "Identity",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
