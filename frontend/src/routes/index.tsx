1eff1c8e4831309d66056a327a0c31c6b329e14d
AleGxrcia <fagarciatavarez@gmail.com>
2025-07-21 23:59:21 -0400

    feat(auth): configure axios interceptors for token handling

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,8 @@ import { Layout } from "../components/layout/Layout";
 import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
 import MoviesListPage from "../pages/MoviesListPage";
+import RegisterPage from "../features/auth/pages/RegisterPage";
+import LoginPage from "../features/auth/pages/LoginPage";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +14,8 @@ const router = createBrowserRouter([
         children: [
             { index: true, element: <HomePage /> },
             { path: 'movies', element: <MoviesListPage /> },
+            { path: 'login', element: <LoginPage /> },
+            { path: 'register', element: <RegisterPage /> },
         ],
     },
 ]);
