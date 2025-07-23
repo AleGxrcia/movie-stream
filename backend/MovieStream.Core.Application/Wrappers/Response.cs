2eba38a5bb34018e674495193e4fc2aa435457c4
AleGxrcia <fagarciatavarez@gmail.com>
2025-07-23 16:38:50 -0400

    add base Response and Error classes for API handling

--- backend/MovieStream.Core.Application/Wrappers/Response.cs
+++ backend/MovieStream.Core.Application/Wrappers/Response.cs
@@ -1,35 +1,39 @@
 ﻿namespace MovieStream.Core.Application.Wrappers
 {
-    public class Response<T>
+    public class Response
     {
         public bool Succeeded { get; set; }
         public string? Message { get; set; }
         public Error? Error { get; set; }
-        public T Data { get; set; }
-        public MetaData? Meta { get; private set; }
 
-        public Response() { }
-
-        public Response(T data, string? message = null)
+        public Response(bool succeeded, string? message = null)
         {
-            Succeeded = true;
+            Succeeded = succeeded;
             Message = message;
-            Data = data;
-            Meta = null;
-        }
-
-        public Response(T data, MetaData meta, string? message = null)
-        {
-            Succeeded = true;
-            Message = message;
-            Data = data;
-            Meta = meta;
         }
 
         public Response(Error error)
         {
             Succeeded = false;
             Error = error;
+        }
+    }
+
+    public class Response<T> : Response
+    {
+        public T Data { get; set; }
+        public MetaData? Meta { get; private set; }
+
+        public Response(T data, string? message = null) : base(true, message)
+        {
+            Data = data;
+            Meta = null;
+        }
+
+        public Response(T data, MetaData meta, string? message = null) : base(true, message)
+        {
+            Data = data;
+            Meta = meta;
         }
     }
 
